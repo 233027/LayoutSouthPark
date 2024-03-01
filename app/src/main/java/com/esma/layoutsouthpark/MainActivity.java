053e2b34f2f9eebe7b868c42c42d66fee101746c
Esma <nurdnc20@gmail.com>
2024-03-01 10:15:46 +0300

    Mobile Programming Java Learning

---
+++ app/src/main/java/com/esma/layoutsouthpark/MainActivity.java
@@ -0,0 +1,14 @@
+package com.esma.layoutsouthpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
